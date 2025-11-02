6040b71609ab45382607bdadb5da03db799479f7
Danilo Abreu <danilo.abreu@redemobconsorcio.com.br>
2025-11-02 12:59:31 -0300

    finalização da poc

---
+++ src/app/components/verificar-email/verificar-email.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificar-email',
+  imports: [],
+  templateUrl: './verificar-email.html',
+  styleUrl: './verificar-email.css',
+})
+export class VerificarEmail {
+
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  auth0: {
+    domain: 'dev-75u3tgtrzvndx1o0.us.auth0.com',
+    clientId: 'bG0qIsZx3GkCVF5RWJosI9TkEW8caqOI'
+  }
+};
